f5f1e01a1cf18d8d0f830c052f430323273b207c
Альберт Олег Витальевич <oleg.albert.99@mail.ru>
2024-05-31 18:41:03 +0300

    Практическое задание Л4

--- src/Animals/Bowl.java
+++ src/Animals/Bowl.java
@@ -4,13 +4,24 @@ public class Bowl {
     private int food;
 
     public Bowl(int food) {
-        this.food = food;
+        if (food > 0) {
+            this.food = food;
+        }
+        else {
+            food = 0;
+            System.out.println("Нельзя добавить в миску отрицательное число еды.");
+        }
     }
 
     public void addFood(int amount) {
-        this.food += amount;
-        System.out.println("В миску добавлено " + amount + " еды. В миске осталось " + this.food + " еды.");
+        if (amount > 0) {
+            this.food += amount;
+            System.out.println("В миску добавлено " + amount + " еды. В миске осталось " + this.food + " еды.");
+        } else {
+            System.out.println("Нельзя добавить в миску отрицательное число еды.");
+        }
     }
+
 
     public int getFood() {
         return food;
