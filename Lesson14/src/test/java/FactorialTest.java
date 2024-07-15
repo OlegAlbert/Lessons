8628f69575282c7aee2de5a2e964548225fc9637
Альберт Олег Витальевич <oleg.albert.99@mail.ru>
2024-07-15 21:08:52 +0300

    Практическое задание Л14 (testng)

--- Lesson14/src/test/java/FactorialTest.java
+++ Lesson14/src/test/java/FactorialTest.java
@@ -1,50 +1,46 @@
 import org.example.Factorial;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
 
 public class FactorialTest {
+
     @Test
     public void testFactorialOfZero() {
-        assertEquals(1, Factorial.computeFactorial(0));
+        assertEquals(Factorial.computeFactorial(0), 1);
     }
 
     @Test
     public void testFactorialOfOne() {
-        assertEquals(1, Factorial.computeFactorial(1));
+        assertEquals(Factorial.computeFactorial(1), 1);
     }
 
     @Test
     public void testFactorialOfTwo() {
-        assertEquals(2, Factorial.computeFactorial(2));
+        assertEquals(Factorial.computeFactorial(2), 2);
     }
 
     @Test
     public void testFactorialOfPositiveNumber() {
-        assertEquals(120, Factorial.computeFactorial(5));
+        assertEquals(Factorial.computeFactorial(5), 120);
     }
 
     @Test
     public void testFactorialOfLargeNumber() {
-        assertEquals(3628800, Factorial.computeFactorial(10));
+        assertEquals(Factorial.computeFactorial(10), 3628800);
     }
 
     @Test
     public void testFactorialOfVeryLargeNumber() {
-        assertEquals(2432902008176640000L, Factorial.computeFactorial(20));
+        assertEquals(Factorial.computeFactorial(20), 2432902008176640000L);
     }
 
-    @Test
+    @Test(expectedExceptions = IllegalArgumentException.class, expectedExceptionsMessageRegExp = "Number must be non-negative")
     public void testFactorialOfNegativeNumber() {
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> Factorial.computeFactorial(-1));
-        assertEquals("Number must be non-negative", exception.getMessage());
+        Factorial.computeFactorial(-1);
     }
 
-    // тест очень долго считает, в итоге выкидывает исключение. Тест проходит
-    /*@Test
+    /*@Test(expectedExceptions = ArithmeticException.class, expectedExceptionsMessageRegExp = "long overflow")
     public void testFactorialOfMaxInt() {
-        Exception exception = assertThrows(ArithmeticException.class, () -> {
-            Factorial.computeFactorial(Integer.MAX_VALUE);
-        });
-        assertTrue(exception.getMessage().contains("long overflow"));
+        Factorial.computeFactorial(Integer.MAX_VALUE);
     }*/
 }
