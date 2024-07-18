263686d9da2a4dde1431755cd19cfb8829f3cb74
Альберт Олег Витальевич <oleg.albert.99@mail.ru>
2024-07-18 16:29:35 +0300

    Lesson_16

--- src/test/java/MtsTests.java
+++ src/test/java/MtsTests.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,8 +11,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,8 +21,13 @@ public class MtsTests {
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("http://mts.by");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.jsReturnsValue("return document.readyState === 'complete';"));
+
+        WebElement cookieWrapper = driver.findElement(By.xpath(
+                "//button[contains(@class, 'cookie__cancel') and contains(@class, 'cookie__cancel')]"));
+        cookieWrapper.click();
     }
 
     @AfterEach
@@ -33,57 +37,216 @@ public class MtsTests {
         }
     }
 
+    //Проверка заполненности полей при выборе "Услуги связи"
     @Test
-    public void testBlockTitle() {
-        WebElement blockTitle = driver.findElement(By.xpath("//h2[normalize-space(text()[1])='Онлайн пополнение' and normalize-space(text()[2])='без комиссии']"));
-        assertNotNull(blockTitle, "Блок 'Онлайн пополнение без комиссии' не найден");
+    public void testEmptyFieldsPlaceholderForConnection() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+
+        WebElement selectArrow = wait.until(ExpectedConditions.visibilityOfElementLocated((By
+                .xpath("//span[contains(@class, 'select__arrow')]"))));
+
+        ((JavascriptExecutor) driver).executeScript(
+                "arguments[0].scrollIntoView(true);", selectArrow);
+        selectArrow.click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(
+                "//p[contains(@class, 'select__option') and text() = 'Услуги связи']")))
+                .click();
+
+        // Пример: Проверка плейсхолдера поля ввода номера телефона
+        WebElement phoneInput = driver.findElement(By.id("connection-phone"));
+        assertEquals("Номер телефона", phoneInput.getAttribute("placeholder"));
+
+        // Добавьте проверки для других полей и вариантов оплаты услуг
+        WebElement sumInput = driver.findElement(By.id("connection-sum"));
+        assertEquals("Сумма", sumInput.getAttribute("placeholder"));
+
+        WebElement emailInput = driver.findElement(By.id("connection-email"));
+        assertEquals("E-mail для отправки чека", emailInput.getAttribute("placeholder"));
     }
 
+    //Проверка заполненности полей при выборе "Домашний интернет"
     @Test
-    public void testPaymentSystemsLogos() {
-        List<WebElement> logos = driver.findElements(By.xpath("//div[contains(@class, 'pay__partners')]//img"));
-        assertTrue(logos.size() > 0, "Платежные системы отсутствуют");
-        for (WebElement logo : logos) {
-            assertTrue(logo.isDisplayed(), "Логотип не отображается");
-        }
+    public void testEmptyFieldsPlaceholderForInternet() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+
+        WebElement selectArrow = wait.until(ExpectedConditions.visibilityOfElementLocated((By
+                .xpath("//span[contains(@class, 'select__arrow')]"))));
+
+        ((JavascriptExecutor) driver).executeScript(
+                "arguments[0].scrollIntoView(true);", selectArrow);
+        selectArrow.click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(
+                "//p[contains(@class, 'select__option') and text() = 'Домашний интернет']")))
+                .click();
+
+        WebElement phoneInput = driver.findElement(By.id("internet-phone"));
+        assertEquals("Номер абонента", phoneInput.getAttribute("placeholder"));
+
+        WebElement sumInput = driver.findElement(By.id("internet-sum"));
+        assertEquals("Сумма", sumInput.getAttribute("placeholder"));
+
+        WebElement emailInput = driver.findElement(By.id("internet-email"));
+        assertEquals("E-mail для отправки чека", emailInput.getAttribute("placeholder"));
     }
 
+    //Проверка заполненности полей при выборе "Рассрочка"
     @Test
-    public void testMoreInfoLink() {
-        WebElement cookieWrapper = driver.findElement(By.xpath("//button[contains(@class, 'cookie__cancel') and contains(@class, 'cookie__cancel')]"));
-        cookieWrapper.click();
+    public void testEmptyFieldsPlaceholderForInstalment() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
 
-        WebElement moreInfoLink = driver.findElement(By.xpath("//a[text()='Подробнее о сервисе']"));
-        moreInfoLink.click();
+        WebElement selectArrow = wait.until(ExpectedConditions.visibilityOfElementLocated((By
+                .xpath("//span[contains(@class, 'select__arrow')]"))));
+        ((JavascriptExecutor) driver).executeScript(
+                "arguments[0].scrollIntoView(true);", selectArrow);
+        selectArrow.click();
 
-        assertEquals("https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/", driver.getCurrentUrl());
+        wait.until(ExpectedConditions.elementToBeClickable(By
+                .xpath("//p[contains(@class, 'select__option') and text() = 'Рассрочка']")))
+                .click();
+
+        WebElement phoneInput = driver.findElement(By.id("score-instalment"));
+        assertEquals("Номер счета на 44", phoneInput.getAttribute("placeholder"));
+
+        WebElement sumInput = driver.findElement(By.id("instalment-sum"));
+        assertEquals("Сумма", sumInput.getAttribute("placeholder"));
+
+        WebElement emailInput = driver.findElement(By.id("instalment-email"));
+        assertEquals("E-mail для отправки чека", emailInput.getAttribute("placeholder"));
     }
 
+    //Проверка заполненности полей при выборе "Задолженность"
     @Test
-    public void testFormSubmission() {
-        WebElement cookieWrapper = driver.findElement(By.xpath("//button[contains(@class, 'cookie__cancel') and contains(@class, 'cookie__cancel')]"));
-        cookieWrapper.click();
+    public void testEmptyFieldsPlaceholderForArrears() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
 
-        WebElement serviceTypeDropdown = driver.findElement(By.xpath("//span[contains(@class, 'select__arrow')]"));
-        serviceTypeDropdown.click();
+        WebElement selectArrow = wait.until(ExpectedConditions.visibilityOfElementLocated((By
+                .xpath("//span[contains(@class, 'select__arrow')]"))));
 
-        serviceTypeDropdown = driver.findElement(By.xpath("//p[contains(@class, 'select__option') and text() = 'Услуги связи']"));
-        serviceTypeDropdown.click();
+        ((JavascriptExecutor) driver).executeScript(
+                "arguments[0].scrollIntoView(true);", selectArrow);
+        selectArrow.click();
 
-        WebElement phoneNumberInput = driver.findElement(By.xpath("//input[contains(@class, 'phone') and @id='connection-phone']"));
-        phoneNumberInput.sendKeys("297777777");
 
-        WebElement sumInput = driver.findElement(By.xpath("//input[contains(@class, 'total_rub') and @id='connection-sum']"));
-        sumInput.sendKeys("50");
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(
+                "//p[contains(@class, 'select__option') and text() = 'Задолженность']")))
+                .click();
 
-        WebElement emailInput = driver.findElement(By.xpath("//input[contains(@class, 'email') and @id='connection-email']"));
-        emailInput.sendKeys("asd@mail.ru");
+        WebElement phoneInput = driver.findElement(By.id("score-arrears"));
+        assertEquals("Номер счета на 2073", phoneInput.getAttribute("placeholder"));
 
-        WebElement continueButton = driver.findElement(By.xpath("//*[@id=\"pay-connection\"]/button"));
+        WebElement sumInput = driver.findElement(By.id("arrears-sum"));
+        assertEquals("Сумма", sumInput.getAttribute("placeholder"));
+
+        WebElement emailInput = driver.findElement(By.id("arrears-email"));
+        assertEquals("E-mail для отправки чека", emailInput.getAttribute("placeholder"));
+    }
+
+    //Проверка заполненности полей в окне подтверждения оплаты
+    @Test
+    public void testFillFormAndVerifyDetails() {
+        String sum = "50.00";
+        String phoneNumber = "297777777";
+        String email = "asd@mail.ru";
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+        WebElement selectArrow = driver.findElement(By
+                .xpath("//span[contains(@class, 'select__arrow')]"));
+        ((JavascriptExecutor) driver).executeScript(
+                "arguments[0].scrollIntoView(true);", selectArrow);
+        selectArrow.click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(
+                "//p[contains(@class, 'select__option') " +
+                        "and text() = 'Услуги связи']"))).click();
+
+        // Заполнение поля номера телефона
+        WebElement phoneInput = driver.findElement(By.id("connection-phone"));
+        phoneInput.sendKeys(phoneNumber);
+
+        WebElement sumInput = driver.findElement(By.id("connection-sum"));
+        sumInput.sendKeys(sum);
+
+        WebElement emailInput = driver.findElement(By.id("connection-email"));
+        emailInput.sendKeys(email);
+
+        WebElement continueButton = driver.findElement(By.xpath("//form[@id='pay-connection']//button"));
         continueButton.click();
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-        WebElement resultForm = wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//div[contains(@class, 'bepaid-app')]"))));
-        assertTrue(resultForm.isDisplayed(), "Не отображается");
+        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(
+                By.xpath("//iframe[contains(@class, 'bepaid-iframe')]")
+        ));
+
+        WebElement sumText = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath(
+                        "//div[contains(@class, 'pay-description__cost')]" +
+                                "/span[contains(text(), 'BYN')]")));
+        assertEquals(sum + " BYN", sumText.getText());
+
+        WebElement phoneNumberText = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//div[contains(@class, 'pay-description__text')]/span")));
+        assertEquals("Оплата: Услуги связи Номер:375" + phoneNumber, phoneNumberText.getText());
+
+        WebElement googlePayButton = wait.until
+                (ExpectedConditions.visibilityOfElementLocated(
+                        By.xpath("//button[@id='gpay-button-online-api-id']")));
+        assertTrue(googlePayButton.isDisplayed());
+
+        WebElement yandexPayButton = wait.until
+                (ExpectedConditions.visibilityOfElementLocated(
+                        By.xpath("//div[@id='yandex-button']/button")));
+        assertTrue(yandexPayButton.isDisplayed());
+
+
+        WebElement cardNumberText = wait.until(ExpectedConditions
+                .visibilityOfElementLocated(By.cssSelector(
+                        ".content input#cc-number + label")));
+        assertEquals("Номер карты", cardNumberText.getText());
+
+        WebElement mastercardIcon = wait.until(ExpectedConditions
+                .visibilityOfElementLocated(By.cssSelector(
+                        "img[src*='mastercard-system.svg']")));
+        assertTrue(mastercardIcon.isDisplayed());
+
+        WebElement visaIcon = wait.until(ExpectedConditions
+                .visibilityOfElementLocated(By.cssSelector(
+                        "img[src*='visa-system.svg']")));
+        assertTrue(visaIcon.isDisplayed());
+
+        WebElement belkartIcon = wait.until(ExpectedConditions
+                .visibilityOfElementLocated(By.cssSelector(
+                        "img[src*='belkart-system.svg']")));
+        assertTrue(belkartIcon.isDisplayed());
+
+        WebElement mirIcon = wait.until(ExpectedConditions
+                .visibilityOfElementLocated(By.cssSelector(
+                        "img[src*='mir-system-ru.svg']")));
+        assertTrue(mirIcon.isDisplayed());
+
+        WebElement maestroIcon = wait.until(ExpectedConditions
+                .visibilityOfElementLocated(By.cssSelector(
+                        "img[src*='maestro-system.svg']")));
+        assertTrue(maestroIcon.isDisplayed());
+
+        WebElement expirationDateText = wait.until(ExpectedConditions
+                .visibilityOfElementLocated(By.cssSelector(
+                        ".content input[formcontrolname='expirationDate'] + label")));
+        assertEquals("Срок действия", expirationDateText.getText());
+
+        WebElement CVCText = wait.until(ExpectedConditions
+                .visibilityOfElementLocated(By.cssSelector(
+                        ".content input[formcontrolname='cvc'] + label")));
+        assertEquals("CVC", CVCText.getText());
+
+        WebElement holderText = wait.until(ExpectedConditions
+                .visibilityOfElementLocated(By.cssSelector(
+                        ".content input[formcontrolname='holder'] + label")));
+        assertEquals("Имя держателя (как на карте)", holderText.getText());
+
+        WebElement sumButton = wait.until(ExpectedConditions
+                .visibilityOfElementLocated(By.xpath(
+                        "//div[contains(@class, 'card-page__card')]/button[contains(text(), 'BYN')]")));
+        assertEquals("Оплатить " + sum + " BYN", sumButton.getText());
     }
 }
